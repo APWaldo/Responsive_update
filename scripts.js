948cf5414303d9142b52f8a7484a6bc1683488ec
APWaldo <apw2131@columbia.edu>
2014-04-08 13:02:22 -0400

    New scripts.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 /**
  * @author
  */
+
+var video = '<iframe width="560" height="315" src="http://www.youtube.com/embed/xoQEjKlZ5lI?html5=1" frameborder="0" allowfullscreen></iframe>';
+
+
+$(document).ready(function() {
+	setNav();
+});
+
+function setNav(){
+	$(".cyclone").on("click", function(){
+	$('#myModal').modal();
+	$(".modal-body").html(video);
+	$('#myModal').on('hidden.bs.modal', function () {
+    $('#myModal iframe').removeAttr('src');
+})
+})};
+
