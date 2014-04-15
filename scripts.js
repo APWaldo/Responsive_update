42c48ee0f704ba0052a2bbf16ad322fc9e738705
APWaldo <apw2131@columbia.edu>
2014-04-15 12:09:00 -0400

    Adding new files.

--- scripts.js
+++ scripts.js
@@ -4,17 +4,26 @@
 
 var video = '<iframe width="560" height="315" src="http://www.youtube.com/embed/xoQEjKlZ5lI?html5=1" frameborder="0" allowfullscreen></iframe>';
 
+var moreInfo ={
+	"cycloneInfo" : "This is more info about the cyclone.",
+	"wonderwheelInfo" : "This is more info about the wonderwheel.",
+	"parachuteInfo" : "This is more info about the parachute.",
+}
 
 $(document).ready(function() {
 	setNav();
 });
 
 function setNav(){
-	$(".cyclone").on("click", function(){
-	$('#myModal').modal();
-	$(".modal-body").html(video);
-	$('#myModal').on('hidden.bs.modal', function () {
-    $('#myModal iframe').removeAttr('src');
+	$(".btn-success").on("click", function(e){
+	console.log(e.target.id);
+	var myID = e.target.id;
+	var propname = myID+"Info";
+	$("#"+myID+"Div").append(moreInfo[propname]);
+	console.log(e.target.parentNode);
+	
+	//$(e.target.parentNode).append(moreInfo[propname])
+	
+	
 })
-})};
-
+};
